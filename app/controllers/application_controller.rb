ce3f9dce853d519411145feb18d18a2d4181d6fb
Fannar Freyr Jonsson <fannar@io.is>
2010-02-10 11:54:38 +0000

    Little hack for memcached. See FIXME: FFJ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,6 +47,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_government
+    Government #FIXME: FFJ: ArgumentError (undefined class/module Government): app/controllers/application_controller.rb:51:in `current_government' AND app/controllers/application_controller.rb:159:in `check_subdomain'
     return @current_government if @current_government
     @current_government = Rails.cache.read('government')
     if not @current_government
